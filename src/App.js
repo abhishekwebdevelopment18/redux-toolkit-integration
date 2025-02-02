42ffd31c48a1f0e67afe2b6fe7802190e1271dc4
abhishekwebdevelopment18 <abhishekwebdevelopment18@gmail.com>
2025-02-02 23:24:17 +0530

    implemented routing and navigation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import "./App.css";
+import Cart from "./components/Cart";
+import Dashboard from "./components/Dashboard";
 import Product from "./components/Product";
+import RootLayout from "./components/RootLayout";
 import "bootstrap/dist/css/bootstrap.min.css";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
 
 function App() {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path={"/"} element={<RootLayout />}>
+        <Route index element={<Dashboard />}></Route>
+        <Route path={"/cart"} element={<Cart />}></Route>
+      </Route>
+    )
+  );
+
   return (
     <div className="App">
-      <Product />
+      <RouterProvider router={router} />
     </div>
   );
 }
